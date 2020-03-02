ceac417f4c7801bbe9569869d11e6779dfc04d4b
Zementaa <51128836+Zementaa@users.noreply.github.com>
2020-03-02 12:01:05 +0100

    Update kniffel.js

--- kniffel.js
+++ kniffel.js
@@ -1,14 +1,15 @@
 var newS;
 var number_of_dice = 0;
 function wuerfeln(){
-	
-	for(var i = 0; i < 5; i++){
-		newS = document.createElement('span');
-		document.body.appendChild(newS);
-		newS.style.color = 'white';
-		newS.id="w"+i;
-		newS.className = 'wuerfel';
-		document.getElementById('w'+i).innerHTML = (Math.floor(Math.random() * 6+1));;
+	if (number_of_dice==0){
+		for(var i = 0; i < 5; i++){
+			newS = document.createElement('span');
+			document.body.appendChild(newS);
+			newS.style.color = 'white';
+			newS.id="w"+i;
+			newS.className = 'wuerfel';
+			document.getElementById('w'+i).innerHTML = (Math.floor(Math.random() * 6+1));;
+		}
 	}
 	
 	number_of_dice++;
